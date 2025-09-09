6dd0fc0a57a3e978ea7d4257d577a6c3cda9761d
Arshdeep singh (prience) <120148646+truepixsel007@users.noreply.github.com>
2025-09-09 00:41:54 -0700

    chatting list create

---
+++ asif/chat/chatting_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ChattingList extends StatefulWidget {
+  const ChattingList({super.key});
+
+  @override
+  State<ChattingList> createState() => _ChattingListState();
+}
+
+class _ChattingListState extends State<ChattingList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chatting List'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -30,13 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,3 +46,5 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
