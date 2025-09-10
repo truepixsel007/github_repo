f2de1ed210bfdc708ef95b7aa5c860484150f29a
Arshdeep singh (prience) <120148646+truepixsel007@users.noreply.github.com>
2025-09-10 00:41:16 -0700

    chat in change

--- asif/chat/chatting_list.dart
+++ asif/chat/chatting_list.dart
@@ -18,13 +18,7 @@ class _ChattingListState extends State<ChattingList> {
       body: ListView.builder(
           itemCount: 20,
           itemBuilder: (context, index){
-            return const ListTile(
-               title: Icon(Icons.add),
-            );
-
             return Container();
-
-
       }),
     );
   }
